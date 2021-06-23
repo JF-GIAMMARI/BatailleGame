3957b8a2f3bc21d4bf1975ea4a0391ef38c8c2be
JF-GIAMMARI <giammari.jf@gmail.com>
2021-06-23 11:12:21 +0200

    Init Game Env

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Game from './Game.svelte';
+	let cardPackageLength = 32
+
+</script>
+
+<main>
+	<h1>La Bataille</h1>
+	<Game cardPackageLength={cardPackageLength}/>
+</main>
+
---
+++ src/Game.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let cardPackageLength;
+
+	let cardPackage = new Array()
+	let playerOnePackage = new Array()
+	let playerTwoPackage = new Array()
+	let playerOneName = "Player 1"
+	let playerTwoName = "Player 2"
+	let message = 'Waiting to start'
+
+	function Start(){
+	message = "Game Start"
+	}
+
+	function Stop(){
+	message = "Game is finish"
+	}
+
+	function NextTurn(){
+
+	}
+</script>
+
+
+<p>{message}</p>
+<div class="card">
+	<div class="playerZone">
+		<h5>{playerOneName}</h5>
+		<p>{playerOnePackage.length} cards</p>
+	</div>
+	<div class="playerZone">
+		<h5>{playerTwoName}</h5>	
+		<p>{playerTwoPackage.length} cards </p>
+	</div>
+</div>
+
+
+<div class="buttons">
+	<button on:click={Start}>Start</button>
+	<button on:click={NextTurn}>Next Turn</button>
+	<button on:click={Stop}>Stop</button>
+</div>
