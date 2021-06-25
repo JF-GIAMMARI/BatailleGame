d99b7e9f1a25db12575227d1a2d381f99ec86321
JF-GIAMMARI <giammari.jf@gmail.com>
2021-06-25 13:46:24 +0200

    Add Card Visualisation + Fix Functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
   import Game from "./Game.svelte";
-  let cardPackageLength = 52;
+  let cardPackageLength = 32; //16 / 32 / 52 (16 is dangerous)
   let playerOneName = "Player 1";
   let playerTwoName = "Player 2";
 </script>
 
 <main>
-  <h1>La Bataille</h1>
+  <h1>La Bataille ({cardPackageLength} cartes)</h1>
   <Game {cardPackageLength} {playerOneName} {playerTwoName} />
 </main>
--- src/Game.svelte
+++ src/Game.svelte
@@ -3,58 +3,46 @@
   export let playerOneName;
   export let playerTwoName;
 
-  /**
-   * Card class with value and color
-   */
-  class Card {
-    constructor(value, color) {
-      this.value = value;
-      this.color = color;
-    }
-  }
-
   let cardPackage = new Array();
   let playerOnePackage = new Array();
   let playerTwoPackage = new Array();
   let currentPlayerOneCards = new Array();
   let currentPlayerTwoCards = new Array();
   let message = "Waiting to start";
+  let finalMessage = "";
+
   let turnCount = 0;
+  let isStarted = false;
+  let isPlaced = false;
+  let isAutoPlay = undefined;
+
+  /**
+   * Card class with power value, color and name
+   */
+  class Card {
+    constructor(value, color, name) {
+      this.value = value;
+      this.color = color;
+      this.name = name;
+    }
+  }
 
   /**
    * Initialization sequence of the game
    */
   function start() {
-	playerOnePackage = [];
-    playerTwoPackage = [];
-    cardPackage = [];
-	turnCount = 0
-	if (cardPackage.length == 0) {
-      fillPackage(cardPackage);
-      shufflePackage(cardPackage);
-      splitPackage(cardPackage, playerOnePackage, playerTwoPackage);
-      updateDOM();
-      message = "Game Start";
-    } else {
-      message = "Game already start";
-    }
+    reset();
+    isStarted = true;
+    fillPackage(cardPackage);
+    shufflePackage(cardPackage);
+    splitPackage(cardPackage, playerOnePackage, playerTwoPackage);
+    updateDOM();
+    message = "Game Start";
   }
 
   /**
-   * Display end game message according to the packages
+   * Place cards on waiting zone for the next turn
    */
-  function stop() {
-    let result = comparePackage(playerOnePackage, playerTwoPackage);
-    if (result == 1) {
-      message = playerOneName + " win the game !";
-    } else if (result == -1) {
-      message = playerTwoName + " win the game !";
-    } else {
-      message = "Equality !";
-    }
-
-  }
-
   function nextTurn() {
     turnCount++;
     presentCards(
@@ -63,58 +51,113 @@
       currentPlayerOneCards,
       currentPlayerTwoCards
     );
+    updateDOM();
+    isPlaced = true;
+  }
 
-    if (currentPlayerOneCards.length != 1 && currentPlayerTwoCards != 1) {
+  /**
+   * Compare current cards played in waiting zone and define the action of the players
+   */
+  function compareCurrent() {
+    let referee = compareCard(
+      currentPlayerOneCards[currentPlayerOneCards.length - 1],
+      currentPlayerTwoCards[currentPlayerTwoCards.length - 1]
+    );
+
+    let waitingCardsNb = currentPlayerOneCards.length + currentPlayerTwoCards.length
+
+    if (referee == 1) {
+      giveCards(playerOnePackage);
+      checkPackage();
+      message = playerOneName + " take cards (+"+waitingCardsNb+")";
+    } else if (referee == 0) {
+      message = "Bataille";
       presentCards(
         playerOnePackage,
         playerTwoPackage,
         currentPlayerOneCards,
         currentPlayerTwoCards
       );
-    }
-
-    let referee = compareCard(
-      currentPlayerOneCards[currentPlayerOneCards.length - 1],
-      currentPlayerTwoCards[currentPlayerTwoCards.length - 1]
-    );
-
-    if (referee == 1) {
-      giveCards(playerOnePackage);
-    } else if (referee == 0) {
-      nextTurn();
+      updateDOM();
     } else {
       giveCards(playerTwoPackage);
+      checkPackage();
+      message = playerTwoName + " takes card (+"+waitingCardsNb+")";
+    }
+    updateDOM();
+    isPlaced = false;
+  }
+
+  /**
+   * Display end game message according to the packages
+   */
+  function stop() {
+    clearInterval(isAutoPlay);
+    isStarted = false;
+    let result = comparePackage(playerOnePackage, playerTwoPackage);
+    if (result == 1) {
+      finalMessage = playerOneName + " win the game !";
+    } else if (result == -1) {
+      finalMessage = playerTwoName + " win the game !";
+    } else {
+      finalMessage = "Equality !";
     }
     updateDOM();
   }
 
-  function presentCards(
-    firstPackage,
-    secondPackage,
-    firstBuffer,
-    secondBuffer
-  ) {
-	   checkPackage()
-      firstBuffer.push(new Card(firstPackage[0].value, firstPackage[0].color));
-      firstPackage.shift();
-	  checkPackage()
-      secondBuffer.push(new Card(secondPackage[0].value, secondPackage[0].color)
-      );
-      secondPackage.shift();
-	
+  // UTILITIES FUNCTION
+
+  /**
+   * High speed game simulation
+   */
+  function autoPlay() {
+    if (isAutoPlay == undefined) {
+      isAutoPlay = setInterval(function () {
+        if (isStarted) {
+          nextTurn();
+          compareCurrent();
+        }
+      }, 10);
+    } else {
+      clearInterval(isAutoPlay);
+      isAutoPlay = undefined;
+    }
   }
 
-  function giveCards(targetPackage) {
-    currentPlayerOneCards.forEach((element) => {
-      targetPackage.push(element);
-    });
-    currentPlayerTwoCards.forEach((element) => {
-      targetPackage.push(element);
-    });
+  /**
+   * Self-Assign global array for update SvelteJS DOM
+   */
+  function updateDOM() {
+    playerOnePackage = playerOnePackage;
+    playerTwoPackage = playerTwoPackage;
+    currentPlayerOneCards = currentPlayerOneCards;
+    currentPlayerTwoCards = currentPlayerTwoCards;
+    message = message;
+    finalMessage = finalMessage;
+  }
+
+  // GAMES FUNCTIONS
+
+  /**
+   * Reset game package & values
+   */
+  function reset() {
+    cardPackage = new Array();
+    playerOnePackage = new Array();
+    playerTwoPackage = new Array();
     currentPlayerOneCards = new Array();
     currentPlayerTwoCards = new Array();
+    message = "Waiting to start";
+    finalMessage = "";
+    turnCount = 0;
+    isStarted = false;
+    isPlaced = false;
+    isAutoPlay = undefined;
   }
 
+  /**
+   * Check if all player can continue to play according to the number of cards
+   */
   function checkPackage() {
     if (
       playerOnePackage.length >= cardPackage.length ||
@@ -126,29 +169,42 @@
     }
   }
 
-  // UTILITIES FUNCTION
-
-  /**
-   * Self-Assign global array for update SvelteJS DOM
-   */
-  function updateDOM() {
-    playerOnePackage = playerOnePackage;
-    playerTwoPackage = playerTwoPackage;
-  }
-
   /**
    * Fill the package with card according to the start configuration
    * @param {array} array The array to fill
    */
   function fillPackage(array) {
     let colors = ["hearts", "tiles", "clovers", "pikes"];
-    let values = [2, 3, 4]; //[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-    if (cardPackageLength != 52) {
+    let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    let names = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "Jack",
+      "Queen",
+      "King",
+      "Ace",
+    ];
+
+    if (cardPackageLength == 32) {
       values = values.slice(5);
+      names = names.slice(5);
     }
+
+    if (cardPackageLength == 16) {
+      values = values.slice(9);
+      names = names.slice(9);
+    }
+
     for (let i = 0; i < colors.length; i++) {
       for (let j = 0; j < values.length; j++) {
-        let currentCard = new Card(values[j], colors[i]);
+        let currentCard = new Card(values[j], colors[i], names[j]);
         array.push(currentCard);
       }
     }
@@ -204,11 +260,61 @@
   function compareCard(firstCard, secondCard) {
     if (firstCard.value > secondCard.value) {
       return 1;
-    } else if (firstCard.value == secondCard.value) {
-      return 0;
-    } else {
-      return -1;
     }
+    if (firstCard.value == secondCard.value) {
+      return 0;
+    }
+    return -1;
+  }
+
+  /**
+   * Take cards of two packages in the waiting area
+   * @param {array} firstPackage The first origin package
+   * @param {array} secondPackage The second origin package
+   * @param {array} firstBuffer The first waiting zone
+   * @param {array} secondBuffer The second waiting zone
+   */
+  function presentCards(
+    firstPackage,
+    secondPackage,
+    firstBuffer,
+    secondBuffer
+  ) {
+    if (firstPackage[0] != undefined) {
+      firstBuffer.push(
+        new Card(
+          firstPackage[0].value,
+          firstPackage[0].color,
+          firstPackage[0].name
+        )
+      );
+      firstPackage.shift();
+    }
+    if (secondPackage[0] != undefined) {
+      secondBuffer.push(
+        new Card(
+          secondPackage[0].value,
+          secondPackage[0].color,
+          secondPackage[0].name
+        )
+      );
+      secondPackage.shift();
+    }
+  }
+
+  /**
+   * Give all played cards to a package
+   * @param {array} targetPackage The package
+   */
+  function giveCards(targetPackage) {
+    currentPlayerOneCards.forEach((element) => {
+      targetPackage.push(element);
+    });
+    currentPlayerTwoCards.forEach((element) => {
+      targetPackage.push(element);
+    });
+    currentPlayerOneCards = new Array();
+    currentPlayerTwoCards = new Array();
   }
 </script>
 
@@ -225,8 +331,44 @@
 </div>
 
 <div class="buttons">
-  <button on:click={start}>Start</button>
-  <button on:click={nextTurn}>Next Turn</button>
-  <button on:click={stop}>Stop</button>
+  <button on:click={start} disabled={isStarted}>Start</button>
+  <button on:click={nextTurn} disabled={!isStarted || isPlaced}>Place</button>
+  <button on:click={compareCurrent} disabled={!isStarted || !isPlaced}>
+    Compare
+  </button>
+  <button on:click={stop} disabled={!isStarted}>Stop</button>
+  <button on:click={autoPlay} disabled={!isStarted}>Auto</button>
   <p>{turnCount}</p>
+</div>
+<h1>{finalMessage}</h1>
+
+<div class="card">
+  {#if currentPlayerOneCards[currentPlayerOneCards.length - 1] != undefined}
+    <div
+      class="cardZone {currentPlayerOneCards[currentPlayerOneCards.length - 1]
+        .color}"
+    >
+      {#if currentPlayerOneCards.length % 2 === 0}
+        Hidden card
+      {:else}
+        {currentPlayerOneCards[currentPlayerOneCards.length - 1].color}
+        <br />
+        {currentPlayerOneCards[currentPlayerOneCards.length - 1].name}
+      {/if}
+    </div>
+  {/if}
+  {#if currentPlayerTwoCards[currentPlayerTwoCards.length - 1] != undefined}
+    <div
+      class="cardZone {currentPlayerTwoCards[currentPlayerTwoCards.length - 1]
+        .color}"
+    >
+      {#if currentPlayerTwoCards.length % 2 === 0}
+        Hidden card
+      {:else}
+        {currentPlayerTwoCards[currentPlayerTwoCards.length - 1].color}
+        <br />
+        {currentPlayerTwoCards[currentPlayerTwoCards.length - 1].name}
+      {/if}
+    </div>
+  {/if}
 </div>
