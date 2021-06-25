bf31d65c1bbf1c327c50840e693088cde4eb475a
JF-GIAMMARI <giammari.jf@gmail.com>
2021-06-25 13:45:43 +0200

    Implement Turn

--- src/Game.svelte
+++ src/Game.svelte
@@ -3,7 +3,7 @@
   export let playerOneName;
   export let playerTwoName;
 
-   /**
+  /**
    * Card class with value and color
    */
   class Card {
@@ -16,17 +16,20 @@
   let cardPackage = new Array();
   let playerOnePackage = new Array();
   let playerTwoPackage = new Array();
-  let currentPlayerOneCard = new Card();
-  let currentPlayerTwoCard = new Card();
+  let currentPlayerOneCards = new Array();
+  let currentPlayerTwoCards = new Array();
   let message = "Waiting to start";
-
- 
+  let turnCount = 0;
 
   /**
    * Initialization sequence of the game
    */
-  function Start() {
-    if (cardPackage.length == 0) {
+  function start() {
+	playerOnePackage = [];
+    playerTwoPackage = [];
+    cardPackage = [];
+	turnCount = 0
+	if (cardPackage.length == 0) {
       fillPackage(cardPackage);
       shufflePackage(cardPackage);
       splitPackage(cardPackage, playerOnePackage, playerTwoPackage);
@@ -40,7 +43,7 @@
   /**
    * Display end game message according to the packages
    */
-  function Stop() {
+  function stop() {
     let result = comparePackage(playerOnePackage, playerTwoPackage);
     if (result == 1) {
       message = playerOneName + " win the game !";
@@ -49,13 +52,79 @@
     } else {
       message = "Equality !";
     }
-    playerOnePackage = [];
-    playerTwoPackage = [];
-    cardPackage = [];
-    message = "Waiting to start";
+
   }
 
-  function NextTurn() {}
+  function nextTurn() {
+    turnCount++;
+    presentCards(
+      playerOnePackage,
+      playerTwoPackage,
+      currentPlayerOneCards,
+      currentPlayerTwoCards
+    );
+
+    if (currentPlayerOneCards.length != 1 && currentPlayerTwoCards != 1) {
+      presentCards(
+        playerOnePackage,
+        playerTwoPackage,
+        currentPlayerOneCards,
+        currentPlayerTwoCards
+      );
+    }
+
+    let referee = compareCard(
+      currentPlayerOneCards[currentPlayerOneCards.length - 1],
+      currentPlayerTwoCards[currentPlayerTwoCards.length - 1]
+    );
+
+    if (referee == 1) {
+      giveCards(playerOnePackage);
+    } else if (referee == 0) {
+      nextTurn();
+    } else {
+      giveCards(playerTwoPackage);
+    }
+    updateDOM();
+  }
+
+  function presentCards(
+    firstPackage,
+    secondPackage,
+    firstBuffer,
+    secondBuffer
+  ) {
+	   checkPackage()
+      firstBuffer.push(new Card(firstPackage[0].value, firstPackage[0].color));
+      firstPackage.shift();
+	  checkPackage()
+      secondBuffer.push(new Card(secondPackage[0].value, secondPackage[0].color)
+      );
+      secondPackage.shift();
+	
+  }
+
+  function giveCards(targetPackage) {
+    currentPlayerOneCards.forEach((element) => {
+      targetPackage.push(element);
+    });
+    currentPlayerTwoCards.forEach((element) => {
+      targetPackage.push(element);
+    });
+    currentPlayerOneCards = new Array();
+    currentPlayerTwoCards = new Array();
+  }
+
+  function checkPackage() {
+    if (
+      playerOnePackage.length >= cardPackage.length ||
+      playerTwoPackage.length >= cardPackage.length ||
+      playerOnePackage.length <= 0 ||
+      playerTwoPackage.length <= 0
+    ) {
+      stop();
+    }
+  }
 
   // UTILITIES FUNCTION
 
@@ -73,13 +142,13 @@
    */
   function fillPackage(array) {
     let colors = ["hearts", "tiles", "clovers", "pikes"];
-    let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    let values = [2, 3, 4]; //[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
     if (cardPackageLength != 52) {
       values = values.slice(5);
     }
     for (let i = 0; i < colors.length; i++) {
       for (let j = 0; j < values.length; j++) {
-        let currentCard = new Card(colors[i], values[j]);
+        let currentCard = new Card(values[j], colors[i]);
         array.push(currentCard);
       }
     }
@@ -156,7 +225,8 @@
 </div>
 
 <div class="buttons">
-  <button on:click={Start}>Start</button>
-  <button on:click={NextTurn}>Next Turn</button>
-  <button on:click={Stop}>Stop</button>
+  <button on:click={start}>Start</button>
+  <button on:click={nextTurn}>Next Turn</button>
+  <button on:click={stop}>Stop</button>
+  <p>{turnCount}</p>
 </div>
